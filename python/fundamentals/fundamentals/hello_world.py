cea0aa388ae766bd496f4ed464121152aaa1cba4
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-07-12 14:56:23 +0100

    Function-basics1

---
+++ python/fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,15 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( "Hello Noelle" )	# with a comma
+print("Hello a+" )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( f"Hello{42}" )	# with a comma
+print(#a+)	 # with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( f"I love to eat {fave_food1}and {fave_food2}.") # with .format()
+print( f"i love pizza" ) # with an f string
