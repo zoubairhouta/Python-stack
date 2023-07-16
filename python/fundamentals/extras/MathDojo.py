d6560e2b8d1d3509088447e605c3b5cebec942e7
Ahmed Zoubair Belhout <zoubelhout@gmail.com>
2023-07-16 10:50:38 +0100

    Math-DOjo

---
+++ python/fundamentals/extras/MathDojo.py
@@ -0,0 +1,17 @@
+class MathDojo:
+    def __init__(self):
+    	self.result = 0
+    def add(self, num, *nums):
+        for numbers in nums{
+            result = num+numbers
+        }
+    	# your code here
+    def subtract(self, num, *nums):
+        result
+    	# your code here
+# create an instance:
+md = MathDojo()
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)	# should print 5
+# run each of the methods a few more times and check the result!
